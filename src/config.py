cc8af4f5b2827628a4fc167f08ec8b07348692ca
VitorCarvalhoPinto <vitorcarvalhopinto03@gmail.com>
2024-05-28 12:08:46 -0300

    .

--- src/config.py
+++ src/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", 'postgresql+psycopg2://postgres:1204@flask_db:5432/leilao')
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", 'postgresql+psycopg2://user:password@host:port/db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
